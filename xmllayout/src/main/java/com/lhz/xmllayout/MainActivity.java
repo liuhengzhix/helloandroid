d5d219308f28f698e543fdf36ddfc1436a915140
liuhengzhix <l2731286559@163.com>
2021-03-20 11:25:50 +0800

    添加lambda表达式

--- xmllayout/src/main/java/com/lhz/xmllayout/MainActivity.java
+++ xmllayout/src/main/java/com/lhz/xmllayout/MainActivity.java
@@ -3,12 +3,20 @@ package com.lhz.xmllayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item);
+        setContentView(R.layout.activity_main);
+
+        //找到按钮
+        Button bt = findViewById(R.id.bt_login);
+        //添加点击事件
+        bt.setOnClickListener(v -> Toast.makeText(MainActivity.this, "登录成功", Toast.LENGTH_LONG).show());
     }
 }
