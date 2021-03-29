9db84a749440b82cad903e093c407d89ec40d203
liuhengzhix <l2731286559@163.com>
2021-03-29 08:28:14 +0800

    xml

--- xmllayout/src/main/java/com/lhz/xmllayout/MainActivity.java
+++ xmllayout/src/main/java/com/lhz/xmllayout/MainActivity.java
@@ -2,9 +2,11 @@ package com.lhz.xmllayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,9 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //找到按钮
         Button bt = findViewById(R.id.bt_login);
-        //添加点击事件
-        bt.setOnClickListener(v -> Toast.makeText(MainActivity.this, "登录成功", Toast.LENGTH_LONG).show());
+        EditText name = findViewById(R.id.et_name);
+        EditText pwd = findViewById(R.id.et_pwd);
+        bt.setOnClickListener(v -> {
+            String etName = name.getText().toString();
+            String etPwd = name.getText().toString();
+            if (etName.equals("ls") && etPwd.equals("666")) {
+                Toast.makeText(MainActivity.this, "登录成功", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(MainActivity.this, "用户名或密码错误", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
